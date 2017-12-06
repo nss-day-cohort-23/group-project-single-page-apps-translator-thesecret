f8918c97a558a5c92877c7fc27e7f16ee7b8dc17
Griffin <griffin@Griffins-MacBook-Air.local>
2017-12-06 14:40:20 -0600

    made keys strings wtf?

--- javascripts/finnish.js
+++ javascripts/finnish.js
@@ -1,15 +1,15 @@
 'use strict';
 
 let phrase = {
-  it: 'se',
-  is: 'on',
-  now: 'nyt',
-  time: 'aika',
-  for: 'varten',
-  the: 'että',
-  airing: 'tuuletus',
-  of: 'ja',
-  grievances:'epakohtia'
+  'it': 'se',
+  'is': 'on',
+  'now': 'nyt',
+  'time': 'aika',
+  'for': 'varten',
+  'the': 'että',
+  'airing': 'tuuletus',
+  'of': 'ja',
+  'grievances':'epakohtia'
 }
 
 module.exports = phrase;
